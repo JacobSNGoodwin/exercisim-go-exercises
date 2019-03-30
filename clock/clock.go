530aaa43f8cd0626a770f9a8e25bf3863092e9f5
Jacob Goodwin <jacob.goodwin@gmail.com>
2019-03-29 22:30:11 -0700

    Rev01

--- clock/clock.go
+++ clock/clock.go
@@ -3,13 +3,12 @@ package clock
 
 import "fmt"
 
-// BenchmarkAddMinutes-4           100000000               16.3 ns/op             0 B/op          0 allocs/op
+// BenchmarkAddMinutes-4           100000000               16.2 ns/op             0 B/op          0 allocs/op
 // BenchmarkSubtractMinutes-4      100000000               16.2 ns/op             0 B/op          0 allocs/op
-// BenchmarkCreateClocks-4         200000000                7.52 ns/op            0 B/op          0 allocs/op
+// BenchmarkCreateClocks-4         200000000                7.51 ns/op            0 B/op          0 allocs/op
 
 // Clock stores the hours and minutes of a clock with minutes and hours rolled over
 type Clock struct {
-	h int
 	m int
 }
 
@@ -17,34 +16,26 @@ type Clock struct {
 // It accounts for overflow in hours and minutes along with negative hours
 // and minutes
 func New(h int, m int) Clock {
-	extraHours, minutes := m/60, m%60
-
-	// account for negative number for minutes
+	minutes := (60*h + m) % 1440
+	// 1440 minutes in a day roll over
 	if minutes < 0 {
-		extraHours--
-		minutes += 60
+		minutes += 1440
 	}
 
-	hours := (h + extraHours) % 24
-
-	// account for negative hours
-	if hours < 0 {
-		hours += 24
-	}
-	return Clock{h: hours, m: minutes}
+	return Clock{m: minutes}
 }
 
 // Add adds a minutes to a clock c of type Clock and returns a new Clock
 func (c Clock) Add(a int) Clock {
-	return New(c.h, c.m+a)
+	return New(c.m/60, c.m%60+a)
 }
 
 // Subtract subtracts a minutes to a clock c of type Clock and returns a new Clock
 func (c Clock) Subtract(a int) Clock {
-	return New(c.h, c.m-a)
+	return New(c.m/60, c.m%60-a)
 }
 
 // String converts the clock to a string of format hh:mm
 func (c *Clock) String() string {
-	return fmt.Sprintf("%02d:%02d", c.h, c.m)
+	return fmt.Sprintf("%02d:%02d", c.m/60, c.m%60)
 }
